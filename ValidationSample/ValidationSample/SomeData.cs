68de1bed9376fdc677184a309ccea25e27dd7d0a
Christian Nagel <christiannagel@live.com>
2015-11-19 11:18:51 +0100

    validation

---
+++ ValidationSample/ValidationSample/SomeData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationSample
+{
+    public class SomeData : IDataErrorInfo
+    {
+        private int val1;
+
+        string IDataErrorInfo.this[string columnName]
+        {
+            get
+            {
+                string result = null;
+                switch (columnName)
+                {
+                    case "Val2":
+                        if (Val2 > 50)
+                            result = "error in Val2";
+                        break;
+                    default:
+                        break;
+                }
+
+                return result;
+            }
+        }
+
+        public int Val1
+        {
+            get { return val1; }
+            set
+            {
+                if (value > 50) throw new Exception("bad bad bad value");
+                val1 = value;
+            }
+        }
+
+        public int Val2 { get; set; }
+
+        string IDataErrorInfo.Error
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
