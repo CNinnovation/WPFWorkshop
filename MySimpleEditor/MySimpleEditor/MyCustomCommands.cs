97fc7b54cedf029ddaf8e449b8ccb1293c2c7c01
Christian Nagel <christiannagel@live.com>
2015-11-19 08:30:54 +0100

    initial commit - my simple editor

---
+++ MySimpleEditor/MySimpleEditor/MyCustomCommands.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MySimpleEditor
+{
+    public static class MyCustomCommands
+    {
+        public static RoutedUICommand MyAction { get; } = new RoutedUICommand(nameof(MyAction), nameof(MyAction), typeof(MyCustomCommands));
+    }
+}
