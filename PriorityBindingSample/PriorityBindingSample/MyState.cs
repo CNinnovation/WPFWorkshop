5f957aaa797b867afbff67d63988b823d6d2fd15
Christian Nagel <christiannagel@live.com>
2015-11-19 10:23:23 +0100

    priority binding

---
+++ PriorityBindingSample/PriorityBindingSample/MyState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PriorityBindingSample
+{
+    public class MyState
+    {
+        public string MyCoolData
+        {
+            get
+            {
+                Thread.Sleep(6000);
+                return "finally, the data is here!";
+            }
+        }
+    }
+}
