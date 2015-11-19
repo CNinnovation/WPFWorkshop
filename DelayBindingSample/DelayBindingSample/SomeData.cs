565a67c0817a11e78b1cb88092467be0b4406b32
Christian Nagel <christiannagel@live.com>
2015-11-19 10:32:46 +0100

    updatesourcetrigger, delay binding

---
+++ DelayBindingSample/DelayBindingSample/SomeData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelayBindingSample
+{
+    public class SomeData : INotifyPropertyChanged
+    {
+        private string text;
+
+        public string Text
+        {
+            get { return text; }
+            set
+            {
+                text = value;
+                TextOut = text;
+            }
+        }
+
+        private string _textOut;
+
+        public string TextOut
+        {
+            get { return _textOut; }
+            set
+            {
+                _textOut = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TextOut)));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
