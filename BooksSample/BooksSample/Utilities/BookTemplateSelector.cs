d4649990fbb7d5bda41feec0d0c4228d9c1b8671
Christian Nagel <christiannagel@live.com>
2015-11-19 09:53:05 +0100

    data template selector

---
+++ BooksSample/BooksSample/Utilities/BookTemplateSelector.cs
@@ -0,0 +1,39 @@
+﻿using BooksSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BooksSample.Utilities
+{
+    public class BookTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate WroxBookTemplate { get; set; }
+        public DataTemplate DefaultBookTemplate { get; set; }
+
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            DataTemplate selectedTemplate = null;
+
+            var b = item as Book;
+            if (b != null)
+            {
+                switch (b.Publisher)
+                {
+                    case "Wrox Press":
+                        selectedTemplate = WroxBookTemplate;
+                        break;
+                    default:
+                        selectedTemplate = DefaultBookTemplate;
+                        break;
+                }
+            }
+
+            return selectedTemplate;
+        }
+    }
+}
