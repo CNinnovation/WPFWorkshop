b0da288dc5c3a14a8dbb28cc8edcfdaa03dc6301
Christian Nagel <christiannagel@live.com>
2015-11-19 08:40:16 +0100

    initial commit - BooksSample

---
+++ BooksSample/BooksSample/Models/BooksRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksSample.Models
+{
+    public class BooksRepository
+    {
+        public Book GetTheBook() =>
+            new Book
+            {
+                Isbn = "34784",
+                Title = "Professional C# 5.0",
+                Publisher = "Wrox Press"
+            };
+
+
+        public IEnumerable<Book> GetBooks() =>
+            new List<Book>()
+            {
+                new Book
+                {
+                    Isbn = "34784",
+                    Title = "Professional C# 5.0",
+                    Publisher = "Wrox Press"
+                },
+                new Book
+                {
+                    Isbn = "37258345",
+                    Title = "Enterprise Services",
+                    Publisher = "AWL"
+                },
+                new Book
+                {
+                    Isbn = "5445466",
+                    Title = "Professional C# 6 and .NET Core 5",
+                    Publisher = "Wrox Press"
+                }
+            };
+    }
+}
