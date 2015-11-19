a42370bc11b2dc3533a0be944105cb47693eb26f
Christian Nagel <christiannagel@live.com>
2015-11-19 15:10:27 +0100

    long list sample

--- BooksSampleWithODS/BooksSample/MainWindow.xaml.cs
+++ BooksSampleWithODS/BooksSample/MainWindow.xaml.cs
@@ -1,19 +1,6 @@
 ﻿using BooksSample.Models;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BooksSample
 {
@@ -22,8 +9,6 @@ namespace BooksSample
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Book _theBook;
-        private ObservableCollection<Book> _books;
 
         public MainWindow()
         {
@@ -31,19 +16,5 @@ namespace BooksSample
 
         }
 
-        private void OnShowBook(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(_theBook.Title, _theBook.Publisher);
-        }
-
-        private void OnChangeBook(object sender, RoutedEventArgs e)
-        {
-            _theBook.Title = "Professional C# 6";
-        }
-
-        private void OnAddBook(object sender, RoutedEventArgs e)
-        {
-            _books.Add(new Book { Isbn = "37475", Title = "Programming Windows Apps", Publisher = "Self" });
-        }
     }
 }
---
+++ MyVeryLongList/MyVeryLongList/LongListGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVeryLongList
+{
+    public class LongListGenerator
+    {
+        public IEnumerable<SomeData> GetSomeData()
+        {
+            Random r = new Random();
+            return Enumerable.Range(0, 10000).Select(x => new SomeData { Number = r.Next(500), Text = $"sample {x}" }).ToList();
+        }
+    }
+}
