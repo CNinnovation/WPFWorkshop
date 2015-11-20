2cf28950d9cd7b08f9d3d4a3fd046d8cfc43052c
Christian Nagel <christiannagel@live.com>
2015-11-20 14:57:02 +0100

    shared project sample

---
+++ MySharedDemo/MySharedProject/Demo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace MySharedProject
+{
+    public class Demo
+    {
+        public string Foo(string input)
+        {
+            return input.ToUpper();
+        }
+
+#if WPF
+        public void Bar(string input)
+        {
+            MessageBox.Show(input);
+        }
+#endif
+    }
+}
